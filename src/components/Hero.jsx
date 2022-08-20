d53045906d1dcecc262fb84decf1d10e9d95e1c9
Rinshin Jalal <88965873+Rinshin-Jalal@users.noreply.github.com>
2022-08-20 22:41:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
     <section className="hero">
       <div className="left">
         <h1 className="hero-text">
-           We design <span>The Future!</span> {/*hello this is a test (add ur name here to prove it is successful) */}
+           We design <span>The Future!</span> 
         </h1>
         <button>Explore more</button>
       </div>
       <div className="right">
         <div className="imgs">
-          {/* <img src="/imgs/img-1.png" alt="hero_img1" className="hero-img1" />
-          <img src="/imgs/img-2.png" alt="hero_img2" className="hero-img2" /> */}
+          <img src="/imgs/img-1.png" alt="hero_img1" className="hero-img1" />
+          <img src="/imgs/img-2.png" alt="hero_img2" className="hero-img2" /> 
 
           <img
             src="/imgs/hero-img.png"
