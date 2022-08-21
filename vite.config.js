51ec5ad6809341d90a2ea727c61e50e95ab8cc4a
Rinshin jalal <rinzhin@gmail.com>
2022-08-21 21:27:15 +0530

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  esbuildOptions: {
+    // Node.js global to browser globalThis
+    define: {
+      global: "globalThis", //<-- AWS SDK
+    },
+  },
+});
